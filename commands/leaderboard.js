a3a82b88319b486bf5c1470dceece17c84167da9
letugade <lance@tugade.net>
2022-12-28 12:26:09 -0500

    Finished Leadberboard Functionality

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,7 +4,6 @@ const { SlashCommandBuilder }  = require('discord.js');
 // Leaderboard Embed
 const LeaderboardEmbed = {
 	color: 0x0099ff,
-	title: 'Snowball Bot',
 	description: 'Top Snowballers:\n1.\n2.\n3.'
 };
 
@@ -12,11 +11,13 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName("leaderboard")
     .setDescription("Allows user to view the leaderboard"),
-    async execute(interaction, user, client) {
-        // This is how you reset the description
-        // LeaderboardEmbed.description = "Hello";
-
+    async execute(interaction, users, client) {
         const msg = LeaderboardEmbed;
+        let description = "";
+        for (let i = 0; i < users.length; i++) {
+            description = description + `${i+1}. <@${users[i].user_id}> (${users[i].hits}/${users[i].kos}/${users[i].misses})\n`;
+        }
+        msg.description = description;
         return interaction.reply({ embeds: [msg] });
     }
 }
